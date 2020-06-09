a7d09d3e99ca1f3dd19ce82547209e4bd353dec4
gyim1345 <gyim1345@gmail.com>
2020-06-09 20:20:43 +0900

    Add RaceCalculator
    calculateWinner method to calculate who are the winners
    calculateFurthestPosition to calculate the furthest positioned car

--- src/main/java/step3_racing_car/RaceCalculator.java
+++ src/main/java/step3_racing_car/RaceCalculator.java
@@ -1,9 +1,20 @@
 package step3_racing_car;
 
-public class raceCalculator {
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
-    public static int calculateWinner(int[] carPositions){
+public class RaceCalculator {
 
-        return 1;
+    public static int[] calculateWinner(int[] carPositions){
+        int furthestPosition = calculateFurthestPosition(carPositions);
+        return IntStream.range(0, carPositions.length)
+                .filter(i -> furthestPosition == carPositions[i])
+                .mapToObj(i->i)
+                .mapToInt(x -> x)
+                .toArray();
+    }
+
+    public static int calculateFurthestPosition(int[] carPositions) {
+       return Arrays.stream(carPositions).max().getAsInt();
     }
 }
--- src/test/java/step3_racing_car/RaceCalculatorTest.java
+++ src/test/java/step3_racing_car/RaceCalculatorTest.java
@@ -6,11 +6,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public class RaceCalculator {
+public class RaceCalculatorTest {
 
     @Test
     public void calculateWinnerTest(){
-        RaceCalculator
+        RaceCalculator raceCalculator = new RaceCalculator();
+        assertThat(raceCalculator.calculateWinner(new int[] {4,5,6,6})).isEqualTo(new int[] {2,3});
+    }
+
+    @Test
+    public void calculateFurthestPosition() {
+        RaceCalculator raceCalculator = new RaceCalculator();
+        assertThat(raceCalculator.calculateFurthestPosition(new int[] {4,5,6,6})).isEqualTo(6);
     }
 
 }
