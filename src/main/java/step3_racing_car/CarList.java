1c6b9a7d0fbeee43d5eaaaf4c44c8931e04a2803
gyim1345 <gyim1345@gmail.com>
2020-06-09 20:19:29 +0900

    Add CarList and its test
    getNames method to get names of cars from string
    getIndexNames method to get names from specific index array

--- src/main/java/step3_racing_car/CarList.java
+++ src/main/java/step3_racing_car/CarList.java
@@ -1,4 +1,19 @@
 package step3_racing_car;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class CarList {
+
+    public static String[] getNames(String carNames) {
+        return carNames.split(",");
+    }
+    
+    public static String[] getIndexNames(String[] carListNames, int[] winnersIndex) {
+        return IntStream.range(0, carListNames.length)
+                .filter(i -> IntStream.of(winnersIndex).anyMatch(x-> x== i))
+                .mapToObj(i -> carListNames[i])
+                .toArray(String[]::new);
+    }
 }
