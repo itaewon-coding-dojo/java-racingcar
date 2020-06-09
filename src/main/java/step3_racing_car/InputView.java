4973f92710335342d5f3ccf11148371cf09620c0
gyim1345 <gyim1345@gmail.com>
2020-06-09 20:17:22 +0900

    Edit Views according to the new instructions

--- src/main/java/step3_racing_car/InputView.java
+++ src/main/java/step3_racing_car/InputView.java
@@ -10,6 +10,12 @@ public class InputView {
         return scanner.nextInt();
     }
 
+    public static String carNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
     public static int loopCount() {
         System.out.println("시도할 회수는 몇 회 인가요?");
         scanner = new Scanner(System.in);
